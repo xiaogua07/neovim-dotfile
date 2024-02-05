c80f40f52c89a0fff5823080423b9fb3f38af97b
xiaogua07 <xiaogua07@outlook.com>
2024-02-05 18:56:48 +0800

    init lazy.nvim

--- init.lua
+++ init.lua
@@ -40,3 +40,29 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 -- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
 vim.keymap.set("n", "j", [[ v:count ? 'j' : 'gj' ]], { noremap = true, expr = true })
 vim.keymap.set("n", "k", [[ v:count ? 'k' : 'gk' ]], { noremap = true, expr = true })
+
+-- Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+	lazypath
+    })
+end
+
+set.rtp:prepend(lazypath)
+
+require('lazy').setup({})
+-- End lazy.nvim
+
+
+
+
+
+
+
